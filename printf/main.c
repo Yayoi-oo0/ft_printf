6411b6b7e96545d2f7fa829bef19d35decf3f16b
Okamoto Yayoi <oyayoi@c3r1s6.42tokyo.jp>
2024-08-11 21:06:31 +0900

    main

--- printf/main.c
+++ printf/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okamotoyayoi <okamotoyayoi@student.42.f    +#+  +:+       +#+        */
+/*   By: oyayoi <oyayoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 20:57:52 by oyayoi            #+#    #+#             */
-/*   Updated: 2024/08/05 00:53:07 by okamotoyayo      ###   ########.fr       */
+/*   Updated: 2024/08/11 21:00:48 by oyayoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,91 @@
 
 int	main(void)
 {
-	ft_printf("ft = %x\n", 123);
-	printf("printf = %x\n", 123);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	i = ft_printf("ft     = %p\n", NULL);
+	j = printf("printf = %p\n", NULL);
+	printf("ft_count = %d\n", i);
+	printf("pr_count = %d\n", j);
 	return (0);
 }
+
+// #include "ft_printf.h"
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int num1 = ft_printf("m_c char  > %c            %c  <   ", 'a', 'b');
+//     printf("%d \n", num1);
+//     int num2 = printf("o_c char > %c            %c  <   ", 'a', 'b');
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     static char str = '\0';
+//     num1 = ft_printf("m_c char  > %c            %c  <   ", str, 'b');
+//     printf("%d \n", num1);
+//     num2 = printf("o_c char > %c            %c  <   ", str, 'b');
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_s string    > %s            %s  <   ", "hey", "siri");
+//     printf("%d \n", num1);
+//     num2 = printf("o_s string   > %s            %s  <   ", "hey", "siri");
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_s string    > %s            %s  <   ", "", "\0");
+//     printf("%d \n", num1);
+//     num2 = printf("o_s string   > %s            %s  <   ", "", "\0");
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     int num = 1;
+//     int *p_num1 = &num;
+//     num1 = ft_printf("m_p poiner    > %p %p <   ", "hey", p_num1);
+//     printf("%d \n", num1);
+//     num2 = printf("o_p poiner   > %p %p <   ", "hey", p_num1);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_d digit > %d            %d  <   ", -123, 0);
+//     printf("%d \n", num1);
+//     num2 = printf("o_d digit    > %d            %d  <   ", -123, 0);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_i int   > %i            %i  <   ", -123, 42);
+//     printf("%d \n", num1);
+//     num2 = printf("o_i int  > %i            %i  <   ", -123, 42);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_u uint  > %u            %u  <   ", 1, 4321);
+//     printf("%d \n", num1);
+//     num2 = printf("o_u uint > %u            %u  <   ", 1, 4321);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_x hex   > %x        %x  <   ", -1, 4321);
+//     printf("%d \n", num1);
+//     num2 = printf("o_x hex  > %x        %x  <   ", -1, 4321);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_x hex   > %X        %X  <   ", -1, 42);
+//     printf("%d \n", num1);
+//     num2 = printf("o_x hex  > %X        %X  <   ", -1, 42);
+//     printf("%d \n", num2);
+
+//     printf("\n");
+//     num1 = ft_printf("m_%% %%       > %c            %s  <   ", 'a', "abc");
+//     printf("%d \n", num1);
+//     num2 = printf("o_%% %%      > %c            %s  <   ", 'a', "abc");
+//     printf("%d \n", num2);
+
+//     return (0);
+// }
+
